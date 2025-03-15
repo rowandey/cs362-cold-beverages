989e209675f777ee10491ab847ebf2d49d95df9c
Rowan Dey <rowandey0@oregonstate.edu>
2025-03-15 13:29:33 -0700

    vessel.rb complete

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,20 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume
 
-  def initialize(name, volume)
+  def initialize(name = 'FAKE', volume = 100)
     @name = name
     @volume = volume
+    @container = []
+  end
+
+  def empty?
+    if @container.empty?
+      return true
+    end
+  end
+
+  def fill(input = 'TEST')
+    @container = input
   end
 
 end
